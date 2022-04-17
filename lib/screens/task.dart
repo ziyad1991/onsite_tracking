298b936954efe98642fd195a81ad7b9e8e2113a6
Ziyad Shoeky <mrbrain@msn.com>
2022-04-17 11:19:46 +0200

    new trial

--- lib/screens/task.dart
+++ lib/screens/task.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:image_picker/image_picker.dart1';
 
 
 class TaskScreen extends StatefulWidget {
